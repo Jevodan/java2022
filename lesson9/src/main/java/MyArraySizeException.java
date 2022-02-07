51bbed2b1d69928ea8b9f3aa6ceeeaf5e76350c6
Kracken <sanne80@mail.ru>
2022-02-07 17:35:09 +0300

    Урок 9 готов

---
+++ lesson9/src/main/java/MyArraySizeException.java
@@ -0,0 +1,11 @@
+public class MyArraySizeException extends Exception{
+
+    public MyArraySizeException(String message) {
+        super(message);
+    }
+
+    @Override
+    public String getMessage() {
+        return super.getMessage();
+    }
+}
